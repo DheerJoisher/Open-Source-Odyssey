bd22bc8a6e95b9183e3796f00aa6d3772e46917a
DheerJoisher <dheerjoisher7@gmail.com>
2025-01-24 01:25:53 +0530

    fixed past projects page

--- src/Components/PastProjects.jsx
+++ src/Components/PastProjects.jsx
@@ -3,8 +3,8 @@ import FeatureCard from './FeatureCard'; // Assuming FeatureCard is in the same 
 
 const PastProjects = () => {
   return (
-    <div className="relative h-[175vh] pb-2 w-full bg-slate-900">
-    <div className="absolute inset-0 bgC">
+    <div className="min-h-screen pb-2 w-full bg-slate-900">
+    <div className="inset-0 bgC">
     <div className="text-center overflow-y-auto mt-5">
       <h1 className="text-5xl font-bold text-transparent my-6 py-4 bg-clip-text bg-gradient-to-r from-purple-500 via-blue-500 to-purple-600 mb-10">Past Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
